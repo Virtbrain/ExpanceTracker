e0c447015581cb6d248c6bc7580cecede0dcdcce
AlexeyManankov <virtbrain@gmail.com>
2023-10-13 19:22:27 +0500

    Initial Commit

---
+++ ExpanceTracker/ExpanceTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpanceTrackerApp.swift
+//  ExpanceTracker
+//
+//  Created by Alexey Manankov on 13.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct ExpanceTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
